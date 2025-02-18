05d57d7b516a510fb1d83b65a3735b18077a9b50
Pichsinee Jarusawee <pichsinee.jar@student.mahidol.edu>
2025-02-18 21:54:40 +0700

    updated everything and add main.java

--- HolidayTravelVehicles/Vehicle.java
+++ HolidayTravelVehicles/Vehicle.java
@@ -1,1 +1,33 @@
+public class Vehicle {
+    private String serialNumber;
+    private String name;
+    private String model;
+    private int year;
+    private String manufacturer;
+    private double baseCost;
 
+    public Vehicle(String serialNumber, String name, String model, int year, String manufacturer, double baseCost) {
+        this.serialNumber = serialNumber;
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.manufacturer = manufacturer;
+        this.baseCost = baseCost;
+    }
+
+    public void displayVehicleInfo() {
+        System.out.println("Serial Number: " + serialNumber);
+        System.out.println("Name: " + name);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Manufacturer: " + manufacturer);
+        System.out.println("Base Cost: " + baseCost + " Baht");
+    }
+}
+
+// public class main {
+//     public static void main(String[] args) {
+//         Vehicle vehicle = new Vehicle("V789", "Toyota", "Camry", 2023, "Toyota", 25000.0);
+//         vehicle.displayVehicleInfo(); // Now prints vehicle details
+//     }
+// }
---
+++ HolidayTravelVehicles/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("V789", "Toyota", "Camry", 2023, "Toyota", 25000.0);
+        vehicle.displayVehicleInfo(); 
+    }
+}
